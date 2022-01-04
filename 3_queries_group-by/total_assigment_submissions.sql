e84293fabbd64509eb445e7cffa0a7c03b6055f6
wentingzoe <ywtzoe@gmail.com>
2022-01-03 21:44:34 -0500

    create 6 SQL files.

---
+++ 3_queries_group-by/total_assigment_submissions.sql
@@ -0,0 +1,5 @@
+SELECT cohorts.name as cohort, count(assignment_submissions.*) as total_submissions FROM assignment_submissions
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY cohort
+ORDER BY total_submissions DESC;
